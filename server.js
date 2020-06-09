b491656e395ac573d358b291b0c93bdc3f97a4e3
Courtskit <Courtskit@gmail.com>
2020-06-09 14:37:29 -0700

    lab with notes from class

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ require('dotenv').config();
 const cors = require('cors');
 app.use(cors());
 
+const superagent = require('superagent');
+
 // bring in the PORT through process.env.variable name]
 const PORT = process.env.PORT || 3001;
 
@@ -29,12 +31,29 @@ const PORT = process.env.PORT || 3001;
 //   response.send('I wanna go outside');
 // })
 
+// app.get('/location', (request, response) => {
+//   try {
+//     let search_query = request.query.city;
+//     let geoData = require('./data/location.json');
+//     let returnObj = new Location(search_query, geoData[0]);
+//     response.status(200).send(returnObj);
+//   } catch (err) {
+//     response.status(500).send('Sorry, something went wrong');
+//   }
+// })
+
 app.get('/location', (request, response) => {
   try {
-    let search_query = request.query.city;
-    let geoData = require('./data/location.json');
-    let returnObj = new Location(search_query, geoData[0]);
-    response.status(200).send(returnObj);
+    let city = request.query.city;
+
+    let url = `GET https://us1.locationiq.com/v1/search.php?key=${process.env.GEO_DATA_API_KEY}&q=${city}&format=json`;
+
+    superagent.get(url)
+      .then(results => {
+        let finalObj = new Location(city, results.body[0]);
+        response.status(200).send(finalObj);
+      })
+
   } catch (err) {
     response.status(500).send('Sorry, something went wrong');
   }
@@ -47,27 +66,44 @@ function Location(searchQuery, obj) {
   this.longitude = obj.lon;
 }
 
+// app.get('/weather', (request, response) => {
+
+//   try {
+
+//     let weatherArr = [];
+
+//     let weatherData = require('./data/weather.json');
+//     weatherData.data.forEach(value => {
+//       let weather = new Weather(value);
+//       weatherArr.push(weather);
+//     })
+// INSTEAD USE MAP INSTEAD OF FOR EACH
+
+//     response.status(200).send(weatherArr);
+
+//   } catch (err) {
+//     response.status(500).send('Sorry, something went wrong');
+//   }
+// })
+
 app.get('/weather', (request, response) => {
-
   try {
+    let search_query = request.query.search_query;
 
-    let weatherArr = [];
+    let url = `GET https://api.weatherbit.io/v2.0/current?city=${search_query}&key=${process.env.WEATHER_API_KEY}`;
 
-    let weatherData = require('./data/weather.json');
-    weatherData.data.forEach(value => {
-      let weather = new Weather(value);
-      weatherArr.push(weather);
-    })
-
-    response.status(200).send(weatherArr);
+    superagent.get(url)
+      .then(results => {
+        console.log(results.body);
+      })
 
   } catch (err) {
     response.status(500).send('Sorry, something went wrong');
   }
-
 })
 
 
+7
 // parameter are city and weather info
 function Weather(obj) {
   this.forecast = obj.weather.description;
